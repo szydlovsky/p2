6bdddfacbeb9c0150132495089d87003cb867052
szydlovsky <9szydlowski9@gmail.com>
2021-03-01 21:06:00 +0100

    zad1.2

--- zadanie1C.c
+++ zadanie1C.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 int main()
 {
     const int n = 7;
-    char tab[n][100];
-    for(int i = 0; i < n; i++)
+    char ** tab = new char*[n];
+    for(int i = 0; i < n ; i++ )
     {
-        scanf("%s", (tab[i]));
+        char a[100];
+        scanf("%s", &a);
+        int len = strlen(a);
+        tab[i] = new char[len];
+        for(int j = 0; j < len; j++)
+        {
+            tab[i][j] = a[j];
+        }
     }
+    delete [] tab;
 }
